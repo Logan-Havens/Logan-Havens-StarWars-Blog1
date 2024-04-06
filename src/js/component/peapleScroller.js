7696a40ebb08161bc4b8204dc501bac6a21c00bf
Logan Havens <153969446+Logan-Havens@users.noreply.github.com>
2024-04-06 18:42:24 +0000

    got the people card done

---
+++ src/js/component/peapleScroller.js
@@ -0,0 +1,15 @@
+import React from "react";
+import PeopleCard from "./PeopleCard";
+import "../../styles/home.css";
+const PeopleScroller = () => {
+  return (
+    <div className="container">
+      <div className="">
+        <div className="">
+          <PeopleCard />
+        </div>
+      </div>
+    </div>
+  );
+};
+export default PeopleScroller;
--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -2,78 +2,66 @@ const getState = ({ getStore, getActions, setStore }) => {
 	
 	return {
 		store: {
-			characters: [],
+			people: [],
 			planets: [],
-			vehicles: [],
+			starships: [],
 			favorites: [],
+			currentPerson: {},
+			currentPlanet: {},
+			currentStarship:{},
+
 		},
 		actions: {
-			loadStarWarsPeople: async () => {
-				try {
-					const response = await fetch('https://www.swapi.tech/api/people/')
-					const data = await response.json()
-					if (data?.message !== 'ok') throw Error
-
-					const promises = data.results.map(person => fetch(person.url))
-					const responses = await Promise.all(promises);
-					const finalizedData = await Promise.all(responses.map(response => response.json()));
-					const formattedData = finalizedData.map(response => {
-						return { 
-							description: response.result.description,
-							uid: response.result.uid,
-							...response.result.properties
-						}
-					})
-					setStore({ characters: formattedData })
-				} catch(e) {
-					console.error(e)
-				}
+			fetchPeople: async () => {
+				const response = fetch ("https://www.swapi.tech/api/people/")
+				const data = await response.json() 
+				setStore({people: data.results})
 			},
-			// Finish these functions!
-			loadStarWarsPlanets: async () => {
-				try {
-					const response = await fetch('https://www.swapi.tech/api/planets/')
-					const data = await response.json()
-					if (data?.message !== 'ok') throw Error
-
-					const promises = data.results.map(planets => fetch(planets.url))
-					const responses = await Promise.all(promises);
-					const finalizedData = await Promise.all(responses.map(response => response.json()));
-					const formattedData = finalizedData.map(response => {
-						return { 
-							description: response.result.description,
-							uid: response.result.uid,
-							...response.result.properties
-						}
-					})
-					setStore({ planets: formattedData })
-				} catch(e) {
-					console.error(e)
-				}
+			fetchPlanets: async () => {
+				const response = fetch ("https://www.swapi.tech/api/planets/")
+				const data = await response.json() 
+				setStore({planets: data.results})
 			},
-			loadStarWarsVehicles: async () => {
-				try {
-					const response = await fetch('https://www.swapi.tech/api/vehicles/')
-					const data = await response.json()
-					if (data?.message !== 'ok') throw Error
-
-					const promises = data.results.map(vehicles => fetch(vehicles.url))
-					const responses = await Promise.all(promises);
-					const finalizedData = await Promise.all(responses.map(response => response.json()));
-					const formattedData = finalizedData.map(response => {
-						return { 
-							description: response.result.description,
-							uid: response.result.uid,
-							...response.result.properties
-						}
-					})
-					setStore({ vehicles: formattedData })
-				} catch(e) {
-					console.error(e)
-				}
+			fetchStarships: async () => {
+				const response = fetch ("https://www.swapi.tech/api/starships/")
+				const data = await response.json() 
+				setStore({people: data.results})
 			},
-		}
-	};
+			fetchPersonDetail: async (uid) => {
+				const response = await fetch('https://www.swapi.tech/api/people/${uid}')
+				const data = await response.json();
+				console.log(data)
+				setStore({ currentPerson: data.response.properties});
+		},
+		fetchPlanetDetail: async (uid) => {
+			const response = await fetch('https://www.swapi.tech/api/planets/${uid}')
+			const data = await response.json();
+			console.log(data)
+			setStore({ currentPlanet: data.response.properties});
+
+		},
+		fetchStarshipsDetail: async (uid) => {
+			const response = await fetch('https://www.swapi.tech/api/starships/${uid}')
+			const data = await response.json();
+			console.log(data)
+			setStore({ currentStarship: data.response.properties});
+
+		},
+		addFavorites:(name, uid,type) => {
+			const store = getStore();
+			const newFavorite = {name,uid,type} 
+			const newFavorites = [...store.favorites, newFavorite]
+			setStore({favorites: newFavorites});
+		},
+		removeFavorites:(name) => {
+			const store = getStore();
+			const newFavorites = store.favorites.filter(
+				(favorite) => favorite.name !== name
+			);
+			setStore({favorites: newFavorites});
+		},
+	},
+};
 };
 
 export default getState;
